47b8d26ab6252a73967dd50529466395c379804b
AHTepsilon <alehertoro725@gmail.com>
2022-03-31 20:30:44 -0500

    managed to paint data on page, added stock attribute

--- Ejercicios/Ejercicio_1/index.js
+++ Ejercicios/Ejercicio_1/index.js
@@ -5,26 +5,48 @@ const elementDescription = form.description;
 const elementImage = form.image;
 const elementPrice = form.price;
 const elementColor = form.color;
+const elementStock = form.stock;
 const elementSubmit = document.getElementById("form_element_submit");
+
+let productsArr = [];
+
+const div_products_in_page = document.getElementById("products_area");
 
 form.addEventListener("submit", (ev) =>{
        
         ev.preventDefault();
 
-        /*console.log(elementName.value);
-        console.log(elementDescription.value);
-        console.log(elementImage.value);
-        console.log(elementPrice.value);
-        console.log(elementColor.value);*/
+        div_products_in_page.innerHTML = "";
 
         let product = {
             name: elementName.value,
             description: elementDescription.value,
             image: elementImage.value,
             price: elementPrice.value + ".00$",
-            color: elementColor.value
+            color: elementColor.value,
+            stock: elementStock.checked
         }
-        
 
-        console.log(product);
+        productsArr.push(product);
+        
+        productsArr.forEach((p) => {
+            console.log(p);
+
+            const productDiv = document.createElement("div");
+
+            const availability = p.stock ? "DISPONIBLE" : "NO DISPONIBLE";
+
+            productDiv.innerHTML = 
+            
+            `
+                <h2>${p.name}</h2>
+                <p>${p.description}</p>
+                <p>${p.price}</p>
+                <p>${p.color}</p>
+                <p>${availability}</p>
+            `
+
+            div_products_in_page.appendChild(productDiv);
+        });
+        
 });
